42d88aaa02d815f57c044a58fa12eeb45bb47430
Victor Blacknall <vBeta@localhost.localdomain>
2018-05-24 11:28:07 -0400

    anything3

---
+++ src/itemBox.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react';
+
+
+class ItemBox extends Component{
+
+
+    render(){
+
+
+        return (
+            <div className="item-thing">
+                <h1>itemThing</h1>
+            </div>
+        )
+    }
+
+}
+export default ItemBox;
