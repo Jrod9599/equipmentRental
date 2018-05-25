c39b2ba0d97d8cfb93dc135177db8f4a387dbd84
newsha400 <newsha400@gmail.com>
2018-05-25 11:19:46 -0400

    added store locator

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Login from './Login';
 import Register from './Register';
 import Contact from './Contact';
 import Banner from './banner';
-import Location from './Location';
+import GoogleMap from './GoogleMap';
+import StoreLocator from './StoreLocator'
 
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
@@ -19,7 +20,8 @@ class App extends Component {
         <div className="App">
           <Route path="/" component={BootstrapNavBar}/>
           <Route exact path="/" component={Banner} />
-          <Route exact path="/locations" component={Location} />
+          <Route exact path="/locations" component={StoreLocator} />
+          <Route exact path="/locations" component={GoogleMap} />
           <Route exact path="/login" component={Login}/>
           <Route exact path="/register" component={Register}/>
           <Route exact path="/contact" component={Contact}/>
